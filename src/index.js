0e412b430bde3dbd06fa90a950637db0774c6a43
Shay Keinan <crazypixel@gmail.com>
2017-06-17 17:56:36 +0300

    Pass props

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './style.css';
 
 
-function Header() {
-    return React.createElement('h1', null, 'react and redux course');
+function Header(props) {
+    return React.createElement('h1', null, props.title);
 }
 
 ReactDOM.render(
     React.createElement('div', null, [
         React.createElement('h2', null, 'welcome to'),
-        React.createElement(Header)
+        React.createElement(Header, { title: 'react and redux course' })
     ]),
     document.getElementById('root')
 );

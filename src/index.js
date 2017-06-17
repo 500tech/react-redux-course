7789590e5903a2ed7f17055905213f22de844fdf
Shay Keinan <crazypixel@gmail.com>
2017-06-17 17:07:20 +0300

    init project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+ReactDOM.render(
+    React.createElement('h1', null, 'welcome to react & redux course'),
+    document.getElementById('root')
+);

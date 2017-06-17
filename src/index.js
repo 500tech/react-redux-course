82171dbe99fbd2d816561df90de094c88a875e06
Shay Keinan <crazypixel@gmail.com>
2017-06-17 17:51:10 +0300

    01 - first component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
+
+function Header() {
+    return React.createElement('h1', null, 'react and redux course');
+}
+
 ReactDOM.render(
-    React.createElement('h1', null, 'welcome to react & redux course'),
+    React.createElement('div', null, [
+        React.createElement('h2', null, 'welcome to'),
+        React.createElement(Header)
+    ]),
     document.getElementById('root')
 );

d9bf62640058bba983a1a9da066dcda3da07b8e3
Shay Keinan <crazypixel@gmail.com>
2017-06-20 15:57:26 +0300

    changes

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { toggleFavorite } from '../actions/recipes';
 
-const RecipeDetails = ({ recipe }) => !recipe
-  ? (<h1>Not found</h1>)
-  : (
-  <div className="details">
-    <h3>{ recipe.title }</h3>
+const RecipeDetails = ({recipe, toggleFavorite}) => {
+    if (!recipe) {
+        return <h1>Not found</h1>;
+    }
 
-    <div>
-      <h4>Description</h4>
-      { recipe.description }
-    </div>
-  </div>
-);
+    const classes = cx('toggle-box', {
+        selected: recipe.favorite
+    });
+
+
+    return (
+        <div className="details">
+            <h3>{ recipe.title }</h3>
+
+            <div>
+                <h4>Description</h4>
+                { recipe.description }
+                <div className={ classes } onClick={ () => toggleFavorite(recipe.id) }>favorite</div>
+            </div>
+        </div>
+    )
+};
 
 RecipeDetails.propTypes = {
-  recipe: PropTypes.object
+    recipe: PropTypes.object
 };
 
 const mapStateToProps = (state, ownProps) => ({
-  recipe: state.recipes.find(r => r.id === parseInt(ownProps.match.params.id, 10))
+    recipe: state.recipes.find(r => r.id === parseInt(ownProps.match.params.id, 10))
 });
 
-export default connect(mapStateToProps)(RecipeDetails);
+export default connect(mapStateToProps, { toggleFavorite })(RecipeDetails);
